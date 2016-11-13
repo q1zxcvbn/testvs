11aed0adf40170d7f0c1e45e278fb1af79c13e40
2433275335@qq.com <136860bbabc>
2016-11-13 11:11:10 +0800

    添加项目文件。

---
+++ test_VS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_VS.Startup))]
+namespace test_VS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
